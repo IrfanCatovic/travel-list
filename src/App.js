2cf1f5a5b480edab1cd31279ef6b626ad233eee5
IrfanCatovic <catovicc84@gmail.com>
2023-11-09 14:21:07 +0100

    Adding some style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
       <PackingList />
+      <Stats />
     </div>
   );
 }
 
 function Logo() {
-  return <h1>🏝 Far away 💼</h1>;
+  return <h1>🚢 Far away bag 🧳</h1>;
 }
 
 function Form() {
@@ -21,12 +28,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+          //prvi item je ima komponente, drugi item je prop koji cemo da posaljemo, i treci item u zagradama je objekat koji smo izlistali iz initialItems
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X items on your lsit and you already packed X (X%)</em>
     </footer>
   );
