9767a973e4b0249f1aa33ed514a5ed0545a905ee
IrfanCatovic <catovicc84@gmail.com>
2023-11-15 23:38:30 +0100

    percentage

--- src/App.js
+++ src/App.js
@@ -76,7 +76,8 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
+
       <FlashCards />
     </div>
   );
@@ -178,10 +179,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  //ovo radimo da ne bismo radili useState 2x bez potrebe
+  const numItems = items.length;
+
+  const numPacked = items.filter((item) => item.packed).length;
+
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 📃</em>
+      </p>
+    );
   return (
     <footer className="stats">
-      <em>💼 You have X items on your lsit and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You have got everything! Ready to go ✅ "
+          : `You have ${numItems} on your list, and you already packed X (${percentage}% ))`}
+      </em>
     </footer>
   );
 }
